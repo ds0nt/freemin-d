6d1ed8806f2917c52a6c0a12e3dd6990644b1109
Daniel Sont <dan.sont@gmail.com>
2023-03-21 22:26:37 +0700

    adding to sql as well

--- todo-backend/todos-schema.sql
+++ todo-backend/todos-schema.sql
@@ -9,6 +9,8 @@ CREATE TABLE todos (
   username VARCHAR(25)
     REFERENCES users ON DELETE CASCADE,
   title VARCHAR(50) NOT NULL,
-  habit_description TEXT
+  habit_description TEXT,
+  is_checked BOOLEAN NOT NULL DEFAULT FALSE,
+  last_checked DATE
 );
 
